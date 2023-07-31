eeeaa4136ec3889de8f957b766d11552a264e966
Schyzophrenic <pierre.chastagner@gmail.com>
2023-07-31 21:49:39 +0100

    Add files via upload

---
+++ Tanuk_CodeSequence.lua
@@ -0,0 +1,49 @@
+-----------------------------------------------
+--- Code Sequence for cheat codes    v1.0.0 ---
+--- taken from Toad on discord              ---
+--- https://discord.com/channels/675983554655551509/1078126062753550476/1135205048431951912  ---
+--- and modified by                         ---
+---            (c) Tanuk Prod               ---
+---     https://github.com/Schyzophrenic    ---
+-----------------------------------------------
+
+-- Usage example
+-- self.sprCode = Tanuk_CodeSequence("testCode", {"down", "left", "down", "left", "right", "a"}, function() print("Code Complete") end)
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+class('Tanuk_CodeSequence').extends(gfx.sprite)
+
+--- Creates a Cheat code sequence
+-- @param sequence sequence of input, in order, takes the standard Playdate input up, down, left, right, a, b
+-- @param callbackReward function to call when the code is successful
+-- @param allowMultipleCalls set to false by default. If set to yes, the code may be triggered multiple times. If set to false, it will only be registered once
+function Tanuk_CodeSequence:init(sequence, callbackReward, allowMultipleCalls)
+    self.sequence = sequence
+    self.reward = callbackReward
+    self.allowMultipleCalls = allowMultipleCalls or false
+    self.sequenceIndex = 1
+    self.timerInput = pd.timer.new(500, function() 
+        if not pd.buttonIsPressed(self.sequence[self.sequenceIndex]) and pd.getButtonState() ~= 0 then
+            self.sequenceIndex = 1 
+        end
+    end)
+    self.timerInput.repeats = true
+    self:add()
+end
+
+function Tanuk_CodeSequence:update()
+    if pd.buttonJustPressed(self.sequence[self.sequenceIndex]) then
+        self.sequenceIndex = self.sequenceIndex + 1
+        self.timerInput:reset()
+
+        if self.sequenceIndex > #self.sequence then
+            self:reward()
+            if not self.allowMultipleCalls then
+                self.timerInput:remove()
+                self = nil
+            end
+        end
+    end
+end
